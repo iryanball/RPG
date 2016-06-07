59413fa46c1c0710917f24755f69fce2b78240fe
Ryan Ball <ryanball@icloud.com>
2016-06-07 11:51:02 +0100

    Initial Commit

---
+++ RPG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPG
+//
+//  Created by Ryan Ball on 07/06/2016.
+//  Copyright © 2016 Ryan Ball. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
